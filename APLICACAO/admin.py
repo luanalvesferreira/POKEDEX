99503402f2eea7307c06dd2a8cc6d4c8fbbacd6f
luancslan1 <luanalves367@hotmail.com>
2019-05-02 21:22:14 -0300

    POKEMON CRIADO, CRIAR E FAZER UPLOAD DA FOTO OK

--- APLICACAO/admin.py
+++ APLICACAO/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
-
+admin.site.register(Habilidade)
+admin.site.register(Categoria)
+admin.site.register(Pokemon)
--- APLICACAO/views.py
+++ APLICACAO/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pokemon
 
 # Create your views here.
+
+def mostrarPokemons(request):
+    pokemons = Pokemon.objects.all()
+
+    contexto = {
+        "todos_pokemons": pokemons
+    }
+    return render(request,'basico.html',contexto)
